a08b816e528b5e9d243b4c0c2b2e28652413aab1
Mohammed Ammar Ammar <m.ammar4web@gmail.com>
2022-09-27 00:57:36 +0300

    Generating our CSS and JavaScript files by WordPress functions.php file.

--- footer.php
+++ footer.php
@@ -1,9 +1,4 @@
-<!-- Core JavaScript
-    ================================================== -->
-<script src="js/jquery.min.js"></script>
-<script src="js/tether.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/custom.js"></script>
+<?php wp_footer(); ?>
 
 </body>
 
