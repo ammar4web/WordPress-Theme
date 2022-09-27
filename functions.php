007b72a73cb7851512cbb2043324c6c01324709a
Mohammed Ammar Ammar <m.ammar4web@gmail.com>
2022-09-27 07:34:28 +0300

    Notes about the code of the last commit.

--- functions.php
+++ functions.php
@@ -3,18 +3,23 @@
 function load_assets()
 {
 
+    // get_template_directory_uri()
     $base = get_template_directory_uri() . '/';
 
+    // wp_enqueue_style
     wp_enqueue_style('bootstrap', $base . 'assets/css/bootstrap.css', [], false);
     wp_enqueue_style('fontsawesome', $base . 'assets/css/font-awesome.min.css', ['bootstrap'], false);
     wp_enqueue_style('template-style', $base . 'assets/style.css', [], false);
     wp_enqueue_style('template-responsive', $base . 'assets/css/responsive.css', [], false);
     wp_enqueue_style('template-colors', $base . 'assets/css/colors.css', [], false);
 
+    // wp_enqueue_script
     wp_enqueue_script('jq-script', $base . 'assets/js/jquery.min.js', [], false, true);
     wp_enqueue_script('tether-script', $base . 'assets/js/tether.min.js', [], false, true);
     wp_enqueue_script('bootstrap-script', $base . 'assets/js/bootstrap.min.js', [], false, true);
     wp_enqueue_script('custom-script', $base . 'assets/js/custom.js', [], false, true);
 
 }
+// add_action()
+// wp_enqueue_scripts
 add_action('wp_enqueue_scripts', 'load_assets');
