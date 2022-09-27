bb9b716060c5b3225055a1dc2012b85dcca49851
Mohammed Ammar Ammar <m.ammar4web@gmail.com>
2022-09-27 08:39:43 +0300

    Format some Document

--- header.php
+++ header.php
@@ -1,30 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<!-- Basic -->
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<head>
+  <!-- Basic -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-<!-- Mobile Metas -->
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<!-- Site Metas -->
-<title>Cloapedia - Stylish Magazine Blog Template</title>
-<meta name="keywords" content="">
-<meta name="description" content="">
-<meta name="author" content="">
+  <!-- Site Metas -->
+  <title>Cloapedia - Stylish Magazine Blog Template</title>
+  <meta name="keywords" content="">
+  <meta name="description" content="">
+  <meta name="author" content="">
 
-<!-- Site Icons -->
-<link rel="shortcut icon" href="assets/images/favicon.ico" type="image/x-icon" />
-<link rel="apple-touch-icon" href="assets/images/apple-touch-icon.png">
+  <!-- Site Icons -->
+  <link rel="shortcut icon" href="assets/images/favicon.ico" type="image/x-icon" />
+  <link rel="apple-touch-icon" href="assets/images/apple-touch-icon.png">
 
-<!-- Design fonts -->
-<link href="https://fonts.googleapis.com/css?family=Ubuntu:300,400,400i,500,700" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,400i,500,700,900" rel="stylesheet">
+  <!-- Design fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Ubuntu:300,400,400i,500,700" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,400i,500,700,900" rel="stylesheet">
 
-<?php wp_head(); ?>
+  <?php wp_head(); ?>
 
-<!-- [if lt IE 9]>
+  <!-- [if lt IE 9]>
 <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
 <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 <![endif] -->
--- singular.php
+++ singular.php
@@ -2,4 +2,3 @@
 get_header();
 
 get_footer();
-?>
