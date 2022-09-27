b995f582754124823913b9e0a73e3da6139f8f38
Mohammed Ammar Ammar <m.ammar4web@gmail.com>
2022-09-27 07:45:14 +0300

    functions.php file that I can use for all my website

--- functions.php
+++ functions.php
@@ -1,25 +1,30 @@
-<?php 
+<?php
 
-function load_assets()
-{
-
+if (!function_exists('wpc_load_assets')) {
+  function  wpc_load_assets()
+  {
     // get_template_directory_uri()
     $base = get_template_directory_uri() . '/';
 
     // wp_enqueue_style
-    wp_enqueue_style('bootstrap', $base . 'assets/css/bootstrap.css', [], false);
-    wp_enqueue_style('fontsawesome', $base . 'assets/css/font-awesome.min.css', ['bootstrap'], false);
-    wp_enqueue_style('template-style', $base . 'assets/style.css', [], false);
-    wp_enqueue_style('template-responsive', $base . 'assets/css/responsive.css', [], false);
-    wp_enqueue_style('template-colors', $base . 'assets/css/colors.css', [], false);
+    // Notes: wpc_ id a prifex
+    wp_enqueue_style('wpc_bootstrap', $base . 'assets/css/bootstrap.css', [], false);
+    wp_enqueue_style('wpc_fontsawesome', $base . 'assets/css/font-awesome.min.css', ['wpc_bootstrap'], false);
+    wp_enqueue_style('wpc_template-style', $base . 'assets/style.css', [], false);
+    wp_enqueue_style('wpc_template-responsive', $base . 'assets/css/responsive.css', [], false);
+    wp_enqueue_style('wpc_template-colors', $base . 'assets/css/colors.css', [], false);
 
     // wp_enqueue_script
-    wp_enqueue_script('jq-script', $base . 'assets/js/jquery.min.js', [], false, true);
-    wp_enqueue_script('tether-script', $base . 'assets/js/tether.min.js', [], false, true);
-    wp_enqueue_script('bootstrap-script', $base . 'assets/js/bootstrap.min.js', [], false, true);
-    wp_enqueue_script('custom-script', $base . 'assets/js/custom.js', [], false, true);
-
+    wp_enqueue_script('wpc_jq-script', $base . 'assets/js/jquery.min.js', [], false, true);
+    wp_enqueue_script('wpc_tether-script', $base . 'assets/js/tether.min.js', [], false, true);
+    wp_enqueue_script('wpc_bootstrap-script', $base . 'assets/js/bootstrap.min.js', [], false, true);
+    wp_enqueue_script('wpc_custom-script', $base . 'assets/js/custom.js', [], false, true);
+  }
+} else {
+  // notify the admin by sending emali for example
 }
+
+// --------------------------------------------------------------------------------------------------------
 // add_action()
 // wp_enqueue_scripts
-add_action('wp_enqueue_scripts', 'load_assets');
+add_action('wp_enqueue_scripts', ' wpc_load_assets');
